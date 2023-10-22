d7268ca42ea326a081fd2e83aede6933d50c3d68
Yatharth Verma <yatharthverma070@gmail.com>
2023-10-22 12:35:00 +0530

    feat: Add login, signup, getAllUsers and logout along with cookies feature

--- schemas/user.ts
+++ schemas/user.ts
@@ -1,3 +1,14 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  authentication: {
+    password: { type: String, select: false },
+    access_token: { type: String, select: false },
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 const app = express();
 import "dotenv/config";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import authRouter from "../routes/authentication";
+
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+app.use("/auth", authRouter);
 
 mongoose
   .connect(process.env.MONGODB_URL as string)
